c57a2f3f6c736e50e2b83918d136d8b656446123
roger <rogerfavelron@gmail.com>
2021-07-29 12:25:18 -0700

    first version of api

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
     content:String,
-    author:{type:Schema.Types.ObjectId, ref:'User'}
+    author:{type:Schema.Types.ObjectId, ref:'User'},
+    post:{type:Schema.Types.ObjectId, ref:'Post'}
 })
 
 module.exports = mongoose.model('Comment',CommentSchema);
